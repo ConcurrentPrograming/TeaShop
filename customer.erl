35de210f3bc0c05858e31a158cb4d213f872b70b
kloek <anton_p90@hotmail.com>
2013-10-17 15:27:19 +0200

    hej

--- customer.erl
+++ customer.erl
@@ -1,18 +1,24 @@
 -module(customer).
--export([init_customer/1]).
+-import(main, [getOrderList/0]).
+-export([init_customer/0]).
 
+enterCustomers(0) -> 0;
+enterCustomers(N) when N > 0 ->
+	cust_pid = spawn(customer, init_customer),
+	%% säg hel till ägaren!!!
+	%% wait???
+	enterCustomer(N-1).
 
-
-init_customer(orderList) ->
+init_customer() ->
 	%% N = slumpa antal koppar som denna kund vill dricka
 	N = random:uniform(15) + 1,
 	owner ! {hello, self()},
-	order(N, orderList).
+	order(N).
 
 
-order(0, orderList) ->
+order(0) ->
 	orderList ! {done, self()};
-order(N, order_pid) -> 
-	orderList ! {order, self()},
+order(N) -> 
+	getOrderList() ! {order, self()},
 	%%wait   == tiden det tar o dricka en kopp!
-	order(N-1, orderList).
+	order(N-1).
--- main.erl
+++ main.erl
@@ -1,27 +1,27 @@
 -module(main).
-
--export([start/1]).
+-import(queue,[order/0]).
+-import(owner,[]).
+-import(chef ,[]).
+-import(customer, [enterCustomer/1]).
+-export([start/1, getOrderList/0, getOwner/0, getChef/0]).
 
 
 
 
 start(N) ->
 	register(orderList, spawn(queue, order())),
-    register(owner, spawn(main, XXXXXXXX, [])),   %% XXXXXX == some function!
-    register(chef, spawn(main, XXXXXXXX, [])),    %% XXXXXX == some function!
+    register(owner, spawn(owner, XXXXXXXX, [])),   %% XXXXXX == some function!
+    register(chef, spawn(chef, XXXXXXXX, [])),    %% XXXXXX == some function!
 	enterCustomers(N).
 
+getOrderList() -> orderList.
+
+getOwner() -> owner.
+
+getChef() -> chef.
 
 
 
-
-enterCustomers(0) -> ;
-enterCustomers(N) when N > 0 ->
-	cust_pid = spawn(customer, init_customer, [orderList]),
-	tmp_list = [cust_pid | cust_list ],
-	Cust_list = tmp_list,
-	%% wait???
-	enterCustomer(N-1).
 
 
 
