28c67d9f15034b4945392d756e303a3f2c6372ad
Kioksy <ante89@gmail.com>
2013-10-18 16:56:23 +0200

    test

--- owner.erl
+++ owner.erl
@@ -10,12 +10,13 @@ init_owner() ->
 
 
 work(List) ->
-	io:format("owner is working ~n").
-	%receive
-	%	{hello, PID} ->
-	%		List = [PID | List],
-	%		work(List);
-	%	{bye, PID} -> 
-	%		io:format("hej hej hej då från owner"),   %%%  ta bort pid ur lista!!!
-	%		work(List)
-	%end.
+	io:format("owner is working ~n"),
+	receive
+		{hello, PID} ->
+			io:format("Customer said hello to owner ~n"),
+			List = [PID | List],
+			work(List);
+		{bye, PID} -> 
+			io:format("hej hej hej då från owner"),   %%%  ta bort pid ur lista!!!
+			work(List)
+	end.
