a62ed6ff8478e7301b534056851a0570b1e6d0cd
Kioksy <ante89@gmail.com>
2013-10-21 14:32:07 +0200

    orderlist registered after owner and chef

--- main.erl
+++ main.erl
@@ -6,9 +6,9 @@
 start(0) -> io:format("main:start(0) ");
 start(N) ->
 	io:format("main:start(N) where N=~w, ~p~n", [N, self()]),
-	register(orderList, spawn(fun() -> orderqueue:init_orderqueue() end)),
 	register(owner, spawn(fun() -> owner:init_owner() end)),
 	register(chef, spawn(fun() -> chef:init_chef() end)),
+	register(orderList, spawn(fun() -> orderqueue:init_orderqueue() end)),
 	customer:enterCustomers(N).
 
 getOrderList() -> orderList.
